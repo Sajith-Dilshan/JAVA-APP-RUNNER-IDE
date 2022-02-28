ca9508691175c8fa20dd12de33b145c3e9c49eb7
sajith <sajithd784@gmail.com>
2022-02-28 14:55:05 +0530

    add the template code as a String

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -3,11 +3,25 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextArea;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainFormController {
     public TextArea txtOutput;
     public TextArea txtInput;
 
     public void btnRun_OnAction(ActionEvent actionEvent) {
 
+        /* Step 1 */
+        String data = "public class DEP8IDEDemo{\n" +
+                "public static void main(String args[]){\n"+
+                txtInput.getText() +
+                "\n}\n" +
+                "}";
+
+
+
     }
+
+
 }
